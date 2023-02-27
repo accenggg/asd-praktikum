03f9f07addd0c35ed292f385010d5385b298e2bd
accenggg <arseliusngiklan@gmail.com>
2023-02-27 08:55:15 +0800

    pertemuan2

--- pertemuan2/mergeSort.py
+++ pertemuan2/mergeSort.py
@@ -1,49 +1,51 @@
-def mergerSort(data):
-    if len(data) > 1:
-        mid = len(data) // 2
-        left_data = data[:mid]
-        right_data = data[mid:]
+def merge_sort (array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left_arr = array[:mid]
+        right_arr = array [mid:]
 
-        # print(f"kiri: {left_data}")
-        # print(f"kanan: {right_data}")
+        # print(f"left: {left_arr}")
+        # print(f"right: {right_arr}")
         # print(20*"=")
 
-        mergerSort(left_data)
-        mergerSort(right_data)   
-        
+        # rekursif
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # penggabungan array
         """
-        i = 0 # indeks untuk array kiri
-        j = 0 # indeks untuk array kanan
-        k = 0 # indeks untuk penggabungannya
+        i = 0 # indeks untuk array kiri (left_arr)
+        j = 0 # indeks untuk array kanan (right_arr)
+        k = 0 # indeks untuk penggabungan
         """
 
+        # bisa disingkat dengan
         i = j = k = 0
-
-        while i < len(left_data) and j < len(right_data):
-            if left_data[i] < right_data[j]:
-                data[k] = left_data [i]
-                i += 1
-            else:
-                data[k] = right_data[j]
-                j += 1
-            k += 1
         
-        while i < len(left_data):
-            data[k] = left_data[i]
+        # array kiri dan array kanan masih ada
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                array[k] = left_arr[i]
+                i += 1
+                # k += 1
+            else:
+                array[k] = right_arr[j]
+                j += 1
+                # k += 1
+            k += 1
+
+        # array kiri masih ada namun array kanan sudah habis
+        while (i < len(left_arr)):
+            array[k] = left_arr[i]
             i += 1
             k += 1
-
-        while j < len(right_data):
-            data[k] = right_data[j]
+        
+        # array kanan mmasih ada namun array kiri sudah habis
+        while (j < len(right_arr)):
+            array[k] = right_arr[j]
             j += 1
             k += 1
 
-
-data = [2, 8, 10, 7, 5, 1, 3]
-print(f"sebelum: {data}")
-
-# proses sorting
-mergerSort(data)
-
-# sesudah
-print (f"sesudah: {data}")
+array = [2,3,2,2,5,9,2,1,5,829,204,183,1,3]
+merge_sort (array)
+print(array)
--- pertemuan2/quickSort.py
+++ pertemuan2/quickSort.py
@@ -1,53 +1,22 @@
-# Function to find the partition position
-# Fungsi untuk menemukan posisi partisi
-def partition(array, low, high):
+import os 
+os.system('cls')
 
-	# Memiliki elemen paling kanan sbg pivot
-	pivot = array[high]
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
+	else:
+		pivot = arr[0]
 
-	# Pointer for greater element
-	i = low - 1
+		left = [x for x in arr[1:] if x <= pivot] 
+        
+		right = [x for x in arr[1:] if x >= pivot]
+        
+		return quickSort(left) + [pivot] + quickSort(right)
+    
+arr = [11, 9, 0, 2, 4, 5]
 
-	# Traverse through all elements
-	# compare each element with pivot
-	for j in range(low, high):
-		if array[j] <= pivot:
-			# If element smaller than pivot is found
-			# swap it with the greater element pointed by i
-			i = i + 1
+print(f"List Sebelum di Sort {arr}")
 
-			# Swapping element at i with element at j
-			(array[i], array[j]) = (array[j], array[i])
+result = quickSort(arr)
 
-	# Swap the pivot element with
-	# e greater element specified by i
-	(array[i + 1], array[high]) = (array[high], array[i + 1])
-
-	# Return the position from where partition is done
-	return i + 1
-
-# Function to perform quicksort
-
-
-def quick_sort(array, low, high):
-	if low < high:
-
-		# Find pivot element such that
-		# element smaller than pivot are on the left
-		# element greater than pivot are on the right
-		pi = partition(array, low, high)
-
-		# Recursive call on the left of pivot
-		quick_sort(array, low, pi - 1)
-
-		# Recursive call on the right of pivot
-		quick_sort(array, pi + 1, high)
-
-
-# Driver code
-array = [10, 7, 8, 9, 1, 5]
-quick_sort(array, 0, len(array) - 1)
-
-print(f'Sorted array: {array}')
-
-# This code is contributed by Adnan Aliakbar
+print(f"List setelah di sort {result}")
